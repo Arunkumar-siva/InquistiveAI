ebb097bc062774fcff5f1753f11687588b0a8326
Arularasi J <arularasi.jaya@aspiresys.com>
2025-03-11 09:50:39 +0530

    Applied migration

---
+++ InquistiveAI_Library/Migrations/20250310110940_InquisitiveAIMigration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InquistiveAI_Library.Migrations
+{
+    /// <inheritdoc />
+    public partial class InquisitiveAIMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "AssesmentDetails",
+                newName: "AssessmentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AssessmentId",
+                table: "AssesmentDetails",
+                newName: "Id");
+        }
+    }
+}
